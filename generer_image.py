80d34b8599f5b3775cf5f0760117f37ff53cb7ee
studentgpu <valentin.kervran@gmail.com>
2025-11-26 15:48:38 +0100

    Nouveau commit

--- generer_image.py
+++ generer_image.py
@@ -6,7 +6,7 @@ HF_TOKEN = "" #TODO mettre le  token plus tard dans le git
 
 pipe = StableDiffusionXLPipeline.from_pretrained(
     "stabilityai/stable-diffusion-xl-base-1.0",
-    dtype=torch.float16,
+    torch_dtype=torch.float16,
     use_safetensors=True
 ).to("cuda")
 
@@ -14,15 +14,17 @@ pipe.enable_attention_slicing()
 
 # ---- Chargement du LoRA ----
 # Option 1 : LoRA venant de HuggingFace Hub
-pipe.load_lora_weights("ByteDance/Hyper-SD", weight_name="Hyper-FLUX.1-dev-8steps-lora.safetensors")
-
-# Option 2 : LoRA local
-# pipe.load_lora_weights("./mon_lora", weight_name="lora.safetensors")
+pipe.load_lora_weights("ByteDance/Hyper-SD", weight_name="Hyper-SDXL-2steps-lora.safetensors")
+pipe.load_lora_weights(".", weight_name="minecraft.safetensors")
 
 # Ajuster l’influence du LoRA
-pipe.fuse_lora(lora_scale=0.75)
+pipe.unet_lora_scales = {
+    "Hyper-SD": 0.8,      # 80% d’effet pour l’UNet
+    "minecraft": 0.5      # 50% d’effet pour l’UNet
+}
 
-prompt = "A dragon-dog hybrid spitting computers"
+
+prompt = "A dragon-dog hybrid spitting computers, pixel art"
 
 image = pipe(prompt, guidance_scale=7.5).images[0]
 
--- test_lora_sdxl.py
+++ test_lora_sdxl.py
@@ -5,26 +5,23 @@ from PIL import Image
 # Charger le modèle SDXL
 pipe = StableDiffusionXLPipeline.from_pretrained(
     "stabilityai/stable-diffusion-xl-base-1.0",
-    torch_dtype=torch.float16,
+    torch_dtype = torch.float16,
     use_safetensors=True
 ).to("cuda")
 
-# Remplacer le scheduler (obligatoire pour un LoRA LCM)
-pipe.scheduler = LCMScheduler.from_config(pipe.scheduler.config)
-
 # Charger un LoRA depuis HuggingFace Hub
 pipe.load_lora_weights(
-    "latent-consistency/lcm-lora-sdxl",
-    weight_name="pytorch_lora_weights.safetensors"
+    ".",
+    weight_name="minecraft.safetensors"
 )
 
 # Fusionner le LoRA avec une intensité donnée
 pipe.fuse_lora(lora_scale=0.75)
 
-prompt = "A dragon-dog hybrid spitting computers"
+prompt = "A dragon with a castle"
 
 # LCM = low guidance
-image = pipe(prompt, guidance_scale=1.0, num_inference_steps=6).images[0]
+image = pipe(prompt, guidance_scale=9).images[0]
 
 image.save("ma_premiere_image.png")
 print("Image saved as ma_premiere_image.png")
